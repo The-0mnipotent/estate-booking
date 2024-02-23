bf946c8aa3b58d0b6554b95cc48fda53ccd53d4d
Arpit Singh <arpit84_soe@jnu.ac.in>
2024-02-23 23:53:37 +0530

    Added google Oauth

--- api/routes/Auth.route.js
+++ api/routes/Auth.route.js
@@ -1,7 +1,8 @@
 import express from "express";
-import { signIn, signUp } from "../controllers/Auth.controller.js";
+import { google, signIn, signUp } from "../controllers/Auth.controller.js";
 const authRouter = express.Router();
 
 authRouter.post("/sign-up", signUp);
 authRouter.post("/sign-in", signIn);
+authRouter.post("/google", google);
 export default authRouter;
--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Link, useNavigate } from "react-router-dom";
+import OAuth from "../components/OAuth.jsx";
 import {
   signInFailure,
   signInStart,
@@ -61,7 +62,7 @@ function SignIn() {
         >
           {loading ? "Loading..." : "Sign In"}
         </button>
-        {/* <OAuth/> */}
+        <OAuth />
       </form>
       <div className="flex gap-2 mt-5">
         <p>Dont have an account?</p>
